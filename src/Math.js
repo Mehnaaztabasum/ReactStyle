2d3c41ece7e0382aeb015dc18825b89f15505615
Shaik Mehnaaz Tabasum <71725952+Mehnaaztabasum@users.noreply.github.com>
2022-08-26 15:45:13 +0530

    Commits have done

--- src/Math.js
+++ src/Math.js
@@ -1,1 +1,10 @@
-import React
+const greetings = "Hi Everyone  !!!";
+
+function day() {
+  return "My favourite day is Sunday";
+}
+function month() {
+  return "My favourite Month is April";
+}
+export default greetings;
+export { day, month };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import greetings, { day, month } from "./Math.js";
 
 // using functions
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div style={{ backgroundColor: "black" }}>
+    <h2 style={{ color: "red" }}>{greetings} </h2>
+    <h2 style={{ color: "Green" }}>{day()} </h2>
+    <h2 style={{ color: "Yellow" }}>{month()} </h2>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
 
 //render will take only one html element
 //  {/* <h1 style={{ color: "red" }}>Inline css</h1>   to get inline css in react*/}
