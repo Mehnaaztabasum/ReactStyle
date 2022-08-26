ef4ec0c1155d4688a5aa22721a7b45ba4b68ed4a
Shaik Mehnaaz Tabasum <71725952+Mehnaaztabasum@users.noreply.github.com>
2022-08-26 12:34:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
+const url = "https://picsum.photos/200/200";
+const inline = {
+  color: "red",
+  fontSize: "2rem"
+};
+const date = new Date();
 
-import App from "./App";
+const currtime = date.getHours();
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+let greet = " ";
+if (currtime < 12) {
+  greet = "Good Morning";
+  inline.color = "red";
+} else if (currtime < 18) {
+  greet = "Good afternoon";
+  inline.color = "blue";
+} else {
+  greet = "Good night";
+  inline.color = "green";
+}
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={inline}>
+      {greet}
+    </h1>
+
+    <h1 style={{ color: "red" }}>Inline css</h1>
+
+    <img className="img" src={url + "?grayscale"} />
+  </div>,
+  document.getElementById("root")
 );
+
+//render will take only one html element
+
