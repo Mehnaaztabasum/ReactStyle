862ea75ade062730e56f9c96c5fe279d2233250c
Shaik Mehnaaz Tabasum <71725952+Mehnaaztabasum@users.noreply.github.com>
2022-08-26 13:05:37 +0530

    Create Items.jsx

---
+++ src/Items.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Lists() {
+  return (
+    <div>
+      <img
+        className="img"
+        src={"https://picsum.photos/200/200" + "?grayscale"}
+      />
+    </div>
+  );
+}
+export default Lists;
