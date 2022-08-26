6c159e886e8b157478a81d43f58f34670aa0a98a
Shaik Mehnaaz Tabasum <71725952+Mehnaaztabasum@users.noreply.github.com>
2022-08-26 13:04:53 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./Items";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
+
+/* {/* <h1 className="heading" style={inline}>
+      {greet}
+    </h1>
+    <h1>myname</h1>
+    <h1 style={{ color: "red" }}>Inline css</h1>
+    <img className="img" src={url + "?grayscale"} /> */
